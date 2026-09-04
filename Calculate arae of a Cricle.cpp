1dd28fbf61c06b2de0f7c04da9b33f7b1fb498c7
manikojha77-cmd <manikojha77@gmail.com>
2026-09-04 15:33:34 +0530

    Add files via upload

---
+++ Calculate arae of a Cricle.cpp
@@ -0,0 +1,11 @@
+# include <stdio.h>
+int main()
+{
+	int radius;
+	float pai=3.14,area;
+	printf("Redius of the circle:");
+	scanf("%d",& radius);
+	area=(pai*radius*radius);
+	printf("Area of the circle is:%.2f ",area);
+	return 0;
+}
